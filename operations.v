ee23fa679f0972b4e0c93d5e69db88cfafcdfb75
Rahul Kumawat <rahulkkumawat2002@gmail.com>
2020-08-19 22:00:59 +0530

    Initial commit

---
+++ operations.v
@@ -0,0 +1,8 @@
+`define Inputfile "IMGx.hex" 
+`define Outputfile "output.bmp"
+		
+//`define CONTRAST_OPERATION
+//`define BRIGHTNESS_OPERATION
+//`define INVERT_OPERATION
+`define THRESHOLD_OPERATION 
+
